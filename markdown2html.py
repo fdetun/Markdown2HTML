1ff5a46c1c3885b5bbfb99aefd41cc4efd4f5ab2
fdetun <fouedads@gmail.com>
2020-12-21 02:25:15 +0100

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,16 +1,42 @@
 #!/usr/bin/python3
 """markdown2html"""
 import sys
+import os
+
+
+def writer(code, file):
+    """writer function"""
+    f = open(file, "a+")
+    f.write(str(code) + '\n')
+
+
+def representation(strzero):
+    """repr function"""
+    array = strzero.split(' ')
+    mystr = " ".join(array[1:])
+    return "<h{}>{}</h{}>".format(len(array[0]), mystr, len(array[0]))
+
 
 if __name__ == "__main__":
     """markdown2html"""
 
+    mydict = {}
     if len(sys.argv) != 3:
         print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
         exit(1)
     else:
         try:
-            f = open(sys.argv[1], "r")
+            with open(sys.argv[1], "r") as file:
+                f = str(file.read())
+            try:
+                os.remove(sys.argv[2])
+            except BaseException:
+                pass
+            data = f.split('\n')
+            for i in data:
+                count = i.count("#")
+                codeline = representation(i)
+                writer(codeline, sys.argv[2])
         except FileNotFoundError:
             print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
             exit(1)
