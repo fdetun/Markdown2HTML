7535a0b69c6d6c81a5d58962f92530f89dab6be6
fdetun <fouedads@gmail.com>
2020-12-21 03:14:12 +0100

    dfdf

--- markdown2html.py
+++ markdown2html.py
@@ -10,17 +10,29 @@ def writer(code, file):
     f.write(code)
 
 
-def representation(strzero):
+def representation(strzero, tag):
     """repr function"""
+    ul = "<ul>"
+    if type(strzero) == list:
+        for i in strzero:
+            a = i.split(" ")
+            a = a[1:]
+            ul = ul + '\n' + '<li>' + ' '.join(a) + '</li>'
+        ul = ul + "\n</ul>"
+        return ul
+
     array = strzero.split(' ')
-    mystr = " ".join(array[1:])
-    return "<h{}>{}</h{}>".format(len(array[0]), mystr, len(array[0]))
+    if tag == "#":
+        mystr = " ".join(array[1:])
+        return "<h{}>{}</h{}>".format(len(array[0]), mystr, len(array[0]))
+    else:
+        pass
 
 
 if __name__ == "__main__":
     """markdown2html"""
 
-    mydict = {}
+    arrul = []
     if len(sys.argv) != 3:
         print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
         exit(1)
@@ -35,12 +47,16 @@ if __name__ == "__main__":
             data = f.split('\n')
             for k, i in enumerate(data):
                 if "#" in i:
-                    count = i.count("#")
-                    codeline = representation(i)
+                    codeline = representation(i, "#")
                     if k == len(data) - 1:
                         writer(codeline, sys.argv[2])
                     else:
                         writer(codeline + '\n', sys.argv[2])
+                elif "-" in i:
+                    arrul.append(i)
+            if arrul != []:
+                codeline = representation(arrul, "-")
+                writer(codeline, sys.argv[2])
         except FileNotFoundError:
             print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
             exit(1)
