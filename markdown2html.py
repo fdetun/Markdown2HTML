e04dff399cb101ddca356e491222e167efe0a74f
fdetun <fouedads@gmail.com>
2020-12-21 01:29:05 +0100

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -4,12 +4,12 @@ import sys
 
 
 if len(sys.argv) != 3:
-    print("Usage: ./markdown2html.py README.md README.html")
+    print('Usage: ./markdown2html.py README.md README.html' ,file=sys.stderr)
     exit(1)
 else:
     try:
         f = open(sys.argv[1], "r")
-        exit(0)
     except FileNotFoundError:
-        print('Missing {}'.format(sys.argv[1]))
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         exit(1)
+exit(0)
