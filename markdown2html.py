9d4487bd7322909af8fd31f90e74889114916c79
fdetun <fouedads@gmail.com>
2020-12-21 01:33:49 +0100

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -2,14 +2,16 @@
 """markdown2html"""
 import sys
 
+if __name__ == "__main__":
+    """markdown2html"""
 
-if len(sys.argv) != 3:
-    print('Usage: ./markdown2html.py README.md README.html' ,file=sys.stderr)
-    exit(1)
-else:
-    try:
-        f = open(sys.argv[1], "r")
-    except FileNotFoundError:
-        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+    if len(sys.argv) != 3:
+        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
         exit(1)
-exit(0)
+    else:
+        try:
+            f = open(sys.argv[1], "r")
+        except FileNotFoundError:
+            print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+            exit(1)
+    exit(0)
